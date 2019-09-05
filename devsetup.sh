5d3ad4702ad9f374f089cf5f123ad469673d53a5
Michal Lyskawinski <mscteam99@gmail.com>
2019-09-05 10:06:51 -0400

    * Add pyrepo to .functions - inits a complete py repository * p function to py + handling if there is an arg * Add jnb to .functions - starts jupyter notebook * Add install jupyter to devsetup

--- devsetup.sh
+++ devsetup.sh
@@ -40,7 +40,7 @@ echo "done"
 
 # install some taps
 echo "installing some taps"
-brew install ag cmake ctags tmux vim node reattach-to-user-namespace caskroom/cask/brew-cask
+brew install ag cmake ctags tmux vim node reattach-to-user-namespace caskroom/cask/brew-cask jupyter
 echo "done"
 
 # install some apps"
